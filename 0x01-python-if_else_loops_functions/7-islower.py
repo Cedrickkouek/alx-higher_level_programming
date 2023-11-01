b59b7334a7b4e15d62b9d4c5a17547c49499cb54
cedrickkouek <cedrickkouek@gmail.com>
2023-11-01 23:19:10 +0100

    Checking for lowercase characters

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def islower(c):
+    """This functions checks for a lower case character"""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
