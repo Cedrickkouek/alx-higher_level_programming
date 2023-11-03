b4db97a2a7b53a5ab48620bb92890c3ca05f3961
cedrickkouek <cedrickkouek@gmail.com>
2023-11-03 10:58:04 +0100

    moving

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+    num_args = len(sys.argv) - 1
+    args = sys.argv[1:]
+
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
+        for i, arg in enumerate(args, start=1):
+            print(f"{i}: {arg}")
