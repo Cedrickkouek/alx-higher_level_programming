c9297dfde4ad1d5ae7087390394c00744459a4fd
cedrickkouek <cedrickkouek@gmail.com>
2023-11-21 03:32:25 +0100

    yep

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,12 +4,11 @@ def safe_print_list_integers(my_list=[], x=0):
     ced = 0
     try:
         for i in range(x):
-            try:
-                if type(my_list[i]) ==  int:
-                    print("{:d}".format(my_list[i]), end="")
-                    ced += 1
-            except IndexError:
-                pass
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end="")
+                ced += 1
+    except IndexError:
+        pass
     finally:
         print()
         return ced
