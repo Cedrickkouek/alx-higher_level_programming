4588aaf7e98498d36c8bdff65170a06b7968c6fb
cedrickkouek <cedrickkouek@gmail.com>
2023-11-21 03:06:21 +0100

    Good

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -18,6 +18,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end=' ')
             c += 1
         except IndexError:
-            break
+            pass
     print()
     return c
