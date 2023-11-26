95f55aaa2425553ded81c2f1c32a53d848b3dd7a
cedrickkouek <cedrickkouek@gmail.com>
2023-11-26 16:28:19 +0100

    TDD and Unit testing

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+"""Unittests for max_integer([..])."""
+
 import unittest
-from max_integer import max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
