49db2b4570664cafedbebdfdccad12576dfec8d9
cedrickkouek <cedrickkouek@gmail.com>
2023-11-26 13:05:22 +0100

    Donee

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -32,3 +32,11 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+
+        for _ in range(self.__size):
+            print("#" * self.__size)
